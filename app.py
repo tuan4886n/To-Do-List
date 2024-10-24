e8226733bddef83696cedf4ea54bb7b70e363c35
tuan4886n <tuan4886n@gmail.com>
2024-10-24 19:27:29 +0700

    push .env and update password hasing

--- app.py
+++ app.py
@@ -2,14 +2,18 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from dotenv import load_dotenv
+from werkzeug.security import generate_password_hash, check_password_hash
 import os
 
 load_dotenv()
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your_secret_key'
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'your_secret_key')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SESSION_COOKIE_SECURE'] = True
+app.config['SESSION_COOKIE_HTTPONLY'] = True
+app.config['SESSION_COOKIE_SAMESITE'] = 'Lax'
 
 db = SQLAlchemy(app)
 login_manager = LoginManager()
@@ -21,6 +25,12 @@ class AppUser(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.String(150), nullable=False)
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
 
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -45,7 +55,8 @@ def register():
         if user:
             flash('Username already exists', 'danger')
         else:
-            new_user = AppUser(username=username, password=password)
+            new_user = AppUser(username=username)
+            new_user.set_password(password)
             db.session.add(new_user)
             db.session.commit()
             flash('Registration successful! You can now log in.', 'success')
@@ -58,12 +69,21 @@ def login():
         username = request.form['username']
         password = request.form['password']
         user = AppUser.query.filter_by(username=username).first()
-        if user and user.password == password:
+        if user and user.check_password(password):
             login_user(user)
             return redirect(url_for('index'))
         else:
             flash('Login Unsuccessful. Please check username and password', 'danger')
     return render_template('login.html')
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+    db.session.rollback()
+    return render_template('500.html'), 500
 
 @app.route('/logout')
 @login_required
